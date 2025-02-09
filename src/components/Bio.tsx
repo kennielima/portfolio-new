dd080171db747384ee259616b2216ae7ff88fad5
kennielima <kennylima970@gmail.com>
2025-02-09 11:23:49 +0100

    seo index

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -7,29 +7,28 @@ const Bio = () => {
         <div className='flex flex-col md:w-1/2 md:overflow-scroll h-full justify-between no-scrollbar'>
             <div className='flex flex-col gap-6'>
                 <div className='flex flex-col gap-3'>
-                    <h1 className='text-6xl font-bold'>Kenny Oyewale</h1>
-                    <h3 className='text-2xl'>Software developer</h3>
+                    <h1 className='text-6xl font-bold'>Kennie Oyewale</h1>
+                    <h3 className='text-2xl'>Software Developer</h3>
                 </div>
                 <div className='flex flex-col gap-5'>
                     <p className='text-lg text-neutral-300 leading-tight'>
-                        Hi, I&apos;m Kenny, a software developer with front and backend skills specializing in modern fullstack web technologies. I am currently building a test
+                        Hi, I&apos;m Kennie, a software developer with front and backend skills specializing in building modern fullstack web applications. I am currently building a test
                         <Link
                             href='https://github.com/kennielima/banking-app-system'
                             className='text-neutral-400 hover:text-neutral-500'
                         >
-                            {' '}banking application system{' '}
+                            {' '}banking application system.
                         </Link>
-                        and learning linux fundamentals.
                     </p>
                     <div>
                         <h3 className='font-bold text-lg'>Skills:</h3>
                         <p className='text-neutral-300 text-sm'>Typescript, ReactJs, Node.js, Express, Next.js, MySql, Sequelize, Prisma, PostgreSql, MongoDB, Docker, Firebase, Supabase, Shadcn, AntD, Figma</p>
                     </div>
                     <div className='flex flex-col gap-2'>
-                        <h3 className='font-bold text-lg'>Relevant experience:</h3>
+                        <h3 className='font-bold text-lg'>Recent experience:</h3>
                         <div className=''>
                             <p className='text-neutral-200'>Backend Engineer</p>
-                            <p className='text-neutral-400 text-sm'>Sterling Bank: Nov 2022 - Present</p>
+                            <p className='text-neutral-400 text-sm'>Sterling Bank: Nov 2024 - Present</p>
                         </div>
                         <div className=''>
                             <p className='text-neutral-200'>Frontend Engineer</p>
